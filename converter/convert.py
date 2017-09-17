322c0bea3395c3739935562afddbc13f858ab0e2
shavkunov <mv.shavkunov@yandex.ru>
2017-09-17 16:54:53 +0300

    added fraction translation

--- converter/convert.py
+++ converter/convert.py
@@ -4,25 +4,49 @@ import os
 
 shortcut_regex = r"^\\d"
 
+def transform_fractions(string):
+    # transform my fractions into tex fraction
+    # this is work if fraction on one line
+    pattern = re.compile(r"@([^][]*?)/([^][]*?)@")
+    compiled = pattern.sub(r"\\frac{\1}{\2}", string)
+    return compiled
+
+
 def main():
     path_to_file = sys.argv[1]
     filename, file_extension = os.path.splitext(path_to_file)
     output = "../build/" + filename + ".tex"
 
+    # for testing
+    #output = filename + ".tex"
     replace_data = {}
+
+    replace_data[r" \*"] = r" \\cdot "
+    replace_data[r"~"] = r"\sim "
+    replace_data[r"~~"] = r"\approx "
+    replace_data[r"..."] = r"\dots "
+    replace_data[r"!="] = r"\neq "
+    replace_data[r">="] = r"\ge "
+    replace_data[r"<="] = r"\le "
+    replace_data[r"->"] = r" \rightarrow "
+    replace_data[r"=>"] = r" \Rightarrow "
+    replace_data[r"<=>"] = r" \Leftrightarrow "
+
     with open(output, "w") as dest:
         with open(path_to_file, "r") as source:
             for line in source.readlines():
+                line = transform_fractions(line)
+
                 if line.startswith(" "):
                     spaces = re.match(r"\s*", line).group()
                     line = line.replace(spaces, "")
-                    line = r"\indent" + line + r"\\"
+                    line = r"\indent " + line + r"\\"
                 elif line[0] == "\\" and line[1].isdigit():
-                    parts = line.split(' ')
+                    parts = line.split(" = ", 1)
                     short = parts[0]
-                    value = parts[-1]
+                    value = parts[-1].replace("\n", "")
 
-                    replace_data[short] = value
+                    replace_data[short] = transform_fractions(value)
                     line = ''
                 elif line.startswith("#"):
                     parts = line.split(" ", 1)
@@ -43,18 +67,21 @@ def main():
                 dest.write(line)
 
     # Out files are not big
-    for shortcut in replace_data:
-        # Read in the file
-        with open(output, 'r') as file:
-            filedata = file.read()
+    reversed_data = list(reversed(sorted(replace_data.keys())))  # because \1 and \11 collapses
+    for i in range(1, 5): # Magic. Some shortcuts don't apply properly.
+        for shortcut in reversed_data:
+            # Read in the file
+            with open(output, 'r') as file:
+                filedata = file.read()
 
-        # Replace the target string
-        filedata = filedata.replace(shortcut, replace_data[shortcut])
+            # Replace the target string
+            filedata = filedata.replace(shortcut, replace_data[shortcut])
 
-        # Write the file out again
-        with open(output, 'w') as file:
-            file.write(filedata)
+            # Write the file out again
+            with open(output, 'w') as file:
+                file.write(filedata)
 
+    # TODO: remove this bydlo code
     # Read in the file
     with open(output, 'r') as file:
         filedata = file.read()
@@ -63,6 +90,20 @@ def main():
     empty_string = r"\noindent  \\"
     double_empty = empty_string + '\n' + empty_string
     filedata = filedata.replace(empty_string, "")
+
+    # Write the file out again
+    with open(output, 'w') as file:
+        file.write(filedata)
+
+    # Read in the file
+    with open(output, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("\\\\\n\n\\section", "\n\section")
+    filedata = filedata.replace("\\\\\n\n\\subsection", "\n\subsection")
+    filedata = filedata.replace("\\\\\n\n\\subsubsection", "\n\subsubsection")
+    # filedata = filedata.replace("\n\n\n", "\n")
 
     # Write the file out again
     with open(output, 'w') as file:
