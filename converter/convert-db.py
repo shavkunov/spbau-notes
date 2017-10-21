c7aa29985ab760923d53e8eea9a723f4032052c0
shavkunov <mv.shavkunov@yandex.ru>
2017-10-21 08:04:35 +0300

    i hate tex

---
+++ converter/convert-db.py
@@ -0,0 +1,165 @@
+import re
+import sys
+import os
+
+shortcut_regex = r"^\\d"
+
+
+def transform_grouping_regex(string):
+    # transform my fractions into tex fraction
+    # this is work if fraction on one line
+    fraction_pattern = re.compile(r"@(.*?)/(.*?)@")
+    compiled = fraction_pattern.sub(r"\\frac{\1}{\2}", string) # better for further tex expressions
+
+    sum_pattern = re.compile(r"\\sum from (.*) to (.*?) ")
+    compiled = sum_pattern.sub(r"\\sum \\limits_{\1}^{\2} ", compiled)
+
+    overset_pattern = re.compile(r"\\over{(.*?)} ")
+    compiled = overset_pattern.sub(r"\\overset{\\text{\1}} ", compiled)
+
+    underset_pattern = re.compile(r"\\under{(.*?)} ")
+    compiled = underset_pattern.sub(r"\\underset{\\text{\1}} ", compiled)          
+
+    return compiled
+
+
+def main():
+    path_to_file = sys.argv[1]
+    filename, file_extension = os.path.splitext(path_to_file)
+    output = "../build/" + filename + ".tex"
+
+    # for testing
+    #output = filename + ".tex"
+    replace_data = {}
+
+    replace_data[r" *"] = r" \cdot "
+    replace_data[r" ~"] = r"\sim "
+    replace_data[r" ~~"] = r"\approx "
+    replace_data[r"..."] = r"\dots "
+    replace_data[r" !="] = r"\neq "
+    replace_data[r" ——>"] = r" \rightarrow "
+    replace_data[r" -->"] = r" \rightarrow " # don't ask me about that
+    replace_data[r" <——"] = r" \leftarrow "
+    replace_data[r" <--"] = r" \leftarrow " # don't ask me about that
+    replace_data[r" >="] = r"\ge "
+    replace_data[r" <="] = r"\le "
+    replace_data[r" =>"] = r" \Rightarrow "
+    replace_data[r" <=>"] = r" \Leftrightarrow "
+
+    begin_section = False
+    with open(output, "w") as dest:
+        with open(path_to_file, "rt") as source:
+            tabular = False
+            for line in source.readlines():
+                line = transform_grouping_regex(line)
+
+                if "\\end{tabular" in line and tabular:
+                    tabular = False
+                    dest.write(line)
+                    continue
+
+                if tabular:
+                    items = line.split("&")
+                    items = [ "$\\text{" + item + "}$" for item in items ]
+                    line = " & ".join(items)
+                    line = line.replace("\n", "")
+                    line += " \\\\ \\hline \n"
+
+                if "\\begin{tabular" in line:
+                    tabular = True
+
+                if tabular == True:
+                    dest.write(line)
+                    continue
+
+
+                if "<->" in line:
+                    vertices = line.split(" <-> ", 1)
+                    a = vertices[0]
+                    b = vertices[1].replace("\n", "")
+                    line = a + " -> " + b + "\n" + b + " -> " + a + "\n"
+
+                if "\\begin{" in line or "\\digraph" in line:
+                    begin_section = True
+
+                if line.startswith(" ") and not begin_section:
+                    spaces = re.match(r"\s*", line).group()
+                    line = line.replace(spaces, "")
+                    line = r"\indent " + line + r"\\"
+                elif line[0] == "\\" and line[1].isdigit():
+                    parts = line.split(" = ", 1)
+                    short = parts[0]
+                    value = parts[-1].replace("\n", "")
+
+                    replace_data[short] = transform_grouping_regex(value)
+                    line = ''
+                elif line.startswith("#"):
+                    parts = line.split(" ", 1)
+                    header_type = parts[0]
+                    header = parts[1].replace('\n', '')
+
+                    length = len(header_type)
+                    if length == 1:
+                        line = r"\section*{" + header + r'}' + '\n'
+
+                    if length == 2:
+                        line = r"\subsection*{" + header + r'}' + '\n'
+
+                    if length == 3:
+                        line = r"\subsubsection*{" + header + r'}' + '\n'
+                else:
+                    if not begin_section:
+                        line = r"\noindent " + line.replace('\n', '') + r' \\' + '\n'
+
+                if ((line.startswith("\\end{")) or line == "}\n") and begin_section:
+                    begin_section = False
+
+                dest.write(line)
+
+    # Out files are not big
+    reversed_data = list(reversed(sorted(replace_data.keys())))  # because \1 and \11 collapses
+    for i in range(1, 5): # Magic. Some shortcuts don't apply properly.
+        for shortcut in reversed_data:
+            # Read in the file
+            with open(output, 'r') as file:
+                filedata = file.read()
+
+            # Replace the target string
+            filedata = filedata.replace(shortcut, replace_data[shortcut])
+
+            # Write the file out again
+            with open(output, 'w') as file:
+                file.write(filedata)
+
+    # TODO: remove this bydlo code
+    # Read in the file
+    with open(output, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    empty_string = r"\noindent  \\"
+    double_empty = empty_string + '\n' + empty_string
+    filedata = filedata.replace(empty_string, "")
+
+    # Write the file out again
+    with open(output, 'w') as file:
+        file.write(filedata)
+
+    # Read in the file
+    with open(output, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("\\\\\n\n\\section", "\n\section")
+    filedata = filedata.replace("\\\\\n\n\\subsection", "\n\subsection")
+    filedata = filedata.replace("\\\\\n\n\\subsubsection", "\n\subsubsection")
+    filedata = filedata.replace(r"\*", "*")
+    # filedata = filedata.replace("\n\n\n", "\n")
+
+    # Write the file out again
+    with open(output, 'w') as file:
+        file.write(filedata)
+
+
+if __name__ == "__main__":
+    main()
---
+++ converter/convert-math.py
@@ -0,0 +1,165 @@
+import re
+import sys
+import os
+
+shortcut_regex = r"^\\d"
+
+
+def transform_grouping_regex(string):
+    # transform my fractions into tex fraction
+    # this is work if fraction on one line
+    fraction_pattern = re.compile(r"@(.*?)/(.*?)@")
+    compiled = fraction_pattern.sub(r"\\frac{\1}{\2}", string) # better for further tex expressions
+
+    sum_pattern = re.compile(r"\\sum from (.*) to (.*?) ")
+    compiled = sum_pattern.sub(r"\\sum \\limits_{\1}^{\2} ", compiled)
+
+    overset_pattern = re.compile(r"\\over{(.*?)} ")
+    compiled = overset_pattern.sub(r"\\overset{\\text{\1}} ", compiled)
+
+    underset_pattern = re.compile(r"\\under{(.*?)} ")
+    compiled = underset_pattern.sub(r"\\underset{\\text{\1}} ", compiled)
+
+    if "\\vb{" in compiled:
+        vbox_pattern = re.compile(r"\\vb{(.*?)}")
+        columns = vbox_pattern.findall(compiled)
+        for column in columns:
+            items = column.split(" ")
+            column_text = r"\\begin{pmatrix}"
+            for item in items:
+                column_text += item + r" \\\\ " # omg
+
+            column_text = column_text[:-2] + "\n"
+            column_text += " \\end{pmatrix}\n"
+            compiled = vbox_pattern.sub(column_text, compiled)
+
+    if "\\hb{" in compiled:
+        hbox_pattern = re.compile(r"\\hb{(.*?)}")
+        columns = hbox_pattern.findall(compiled)
+        for column in columns:
+            items = column.split(" ")
+            column_text = r"\\begin{pmatrix}"
+            for item in items:
+                column_text += item + r" & " # omg
+
+            column_text = column_text[:-2] + "\n"
+            column_text += " \\end{pmatrix}\n"
+            compiled = hbox_pattern.sub(column_text, compiled)            
+
+    return compiled
+
+
+def main():
+    path_to_file = sys.argv[1]
+    filename, file_extension = os.path.splitext(path_to_file)
+    output = "../build/" + filename + ".tex"
+
+    # for testing
+    #output = filename + ".tex"
+    replace_data = {}
+
+    replace_data[r" *"] = r" \cdot "
+    replace_data[r" ~"] = r"\sim "
+    replace_data[r" ~~"] = r"\approx "
+    replace_data[r"..."] = r"\dots "
+    replace_data[r" !="] = r"\neq "
+    replace_data[r" >="] = r"\ge "
+    replace_data[r" <="] = r"\le "
+    replace_data[r" —>"] = r" \rightarrow "
+    replace_data[r" ->"] = r" \rightarrow " # don't ask me about that
+    replace_data[r" =>"] = r" \Rightarrow "
+    replace_data[r" <=>"] = r" \Leftrightarrow "
+
+    begin_section = False
+    graph_section = False
+    with open(output, "w") as dest:
+        with open(path_to_file, "rt") as source:
+            for line in source.readlines():
+                if "<->" in line:
+                    vertices = line.split(" <-> ")
+
+                line = transform_grouping_regex(line)
+
+                if "\\begin{" in line and not begin_section:
+                    begin_section = True
+
+                if line.startswith(" ") and not begin_section:
+                    spaces = re.match(r"\s*", line).group()
+                    line = line.replace(spaces, "")
+                    line = r"\indent " + line + r"\\"
+                elif line[0] == "\\" and line[1].isdigit():
+                    parts = line.split(" = ", 1)
+                    short = parts[0]
+                    value = parts[-1].replace("\n", "")
+
+                    replace_data[short] = transform_grouping_regex(value)
+                    line = ''
+                elif line.startswith("#"):
+                    parts = line.split(" ", 1)
+                    header_type = parts[0]
+                    header = parts[1]
+
+                    length = len(header_type)
+                    if length == 1:
+                        line = r"\section*{" + header + r'}' + '\n'
+
+                    if length == 2:
+                        line = r"\subsection*{" + header + r'}' + '\n'
+
+                    if length == 3:
+                        line = r"\subsubsection*{" + header + r'}' + '\n'
+                else:
+                    if not begin_section:
+                        line = r"\noindent " + line.replace('\n', '') + r' \\' + '\n'
+
+                if (line.startswith("\\end{")) and begin_section:
+                    begin_section = False
+                dest.write(line)
+
+    # Out files are not big
+    reversed_data = list(reversed(sorted(replace_data.keys())))  # because \1 and \11 collapses
+    for i in range(1, 5): # Magic. Some shortcuts don't apply properly.
+        for shortcut in reversed_data:
+            # Read in the file
+            with open(output, 'r') as file:
+                filedata = file.read()
+
+            # Replace the target string
+            filedata = filedata.replace(shortcut, replace_data[shortcut])
+
+            # Write the file out again
+            with open(output, 'w') as file:
+                file.write(filedata)
+
+    # TODO: remove this bydlo code
+    # Read in the file
+    with open(output, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    empty_string = r"\noindent  \\"
+    double_empty = empty_string + '\n' + empty_string
+    filedata = filedata.replace(empty_string, "")
+
+    # Write the file out again
+    with open(output, 'w') as file:
+        file.write(filedata)
+
+    # Read in the file
+    with open(output, 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace("\\\\\n\n\\section", "\n\section")
+    filedata = filedata.replace("\\\\\n\n\\subsection", "\n\subsection")
+    filedata = filedata.replace("\\\\\n\n\\subsubsection", "\n\subsubsection")
+    filedata = filedata.replace(r"\*", "*")
+    # filedata = filedata.replace("\n\n\n", "\n")
+
+    # Write the file out again
+    with open(output, 'w') as file:
+        file.write(filedata)
+
+
+if __name__ == "__main__":
+    main()
