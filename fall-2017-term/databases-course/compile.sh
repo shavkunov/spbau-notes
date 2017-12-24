4c1e025bfba182f46d27c7fecc16aeb743ad1ad6
shavkunov <mv.shavkunov@yandex.ru>
2017-12-24 17:10:01 +0300

    added all lections

--- fall-2017-term/databases-course/compile.sh
+++ fall-2017-term/databases-course/compile.sh
@@ -4,11 +4,12 @@
 mkdir build
 cd raw
 for item in *.hw; do
-    python3 "../../../converter/convert.py" $item
+    python3 "../../../converter/convert-db.py" $item
 done
 
 cd ../
 
 #compile whole conspect
-pdflatex main.tex
-open main.pdf
+pdflatex -shell-escape main.tex
+pdflatex -shell-escape main.tex
+pdflatex -shell-escape main.tex
